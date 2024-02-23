c4d45ad4af99e7b00eef81f8bf01abcfecc01dfa
G-Dan-G <dangur1@mail.tau.ac.il>
2024-02-23 22:03:25 +0200

    asm.c now works, still havn't freed all the memory and created demin because I don't have the dick for it today.

--- asm/asm.c
+++ asm/asm.c
@@ -48,7 +48,9 @@
 #define $SP "E"
 #define $RA "F"
 
-FILE * program, *imemin, *dmemin; /*asm.exe program.asm imemin.txt dmemin.txt*/
+FILE * program;
+FILE *imemin;
+FILE *dmemin; /*asm.exe program.asm imemin.txt dmemin.txt*/
 struct labels {
 	char* label_name;
 	int label_address;
@@ -81,15 +83,35 @@ bool is_line_a_label(char* line) { // examples: L4:, Olnmfda:, L1:
 	return false;
 }
 
+
 int save_label(char* name, int address) {
+	char* nam = name;
+	int len = 0;
+	while(*nam != '\0' && *nam != ':'){
+		if (*nam != ' ' && *nam != '\t' && *nam != '\n' && *nam != ':')
+			len++;
+		nam++;
+	}
+	//printf("len: %d\n",len);
+
 	if (label == NULL) {
 		label = (struct labels*)malloc(sizeof(struct labels));
 		if (label == NULL)
 			return EXIT_FAILURE;
 		label->label_address = address;
-		label->label_name = name;
+		label->label_name = (char*)malloc(sizeof(char) * len);
+		if (label->label_name == NULL){return -1;}
+		int i = 0;
+		while (i < len) {
+			if (*name != ' ' && *name != '\t' && *name != '\n' && *name != ':') {
+				label->label_name[i] = *name;
+				i++;
+			}
+			name++;
+		}
+		label->label_name[len] = '\0';
 		label->next = NULL;
-        printf("saved label: %s",name);
+       // printf("saved label: %s\n",label->label_name);
 		return EXIT_SUCCESS;
 	}
 
@@ -102,14 +124,46 @@ int save_label(char* name, int address) {
 			if (tmp->next == NULL)
 				return EXIT_FAILURE;
 			tmp->next->label_address = address;
-			tmp->next->label_name = name;
+			tmp->next->label_name = (char*)malloc(sizeof(char) * len);
+			if (tmp->next->label_name == NULL){return -1;}
+			int i = 0;
+			while (i < len) {
+				if (*name != ' ' && *name != '\t' && *name != '\n' && *name != ':') {
+						tmp->next->label_name[i] = *name;
+						i++;
+					}
+				name++;
+			}
+			tmp->next->label_name[len] = '\0';
 			tmp->next->next = NULL;
-            printf("saved label: %s",name);
+           // printf("saved label: %s\n",tmp->label_name);
 			return EXIT_SUCCESS;
 		}
 		tmp = tmp->next;
 	}
 	return EXIT_FAILURE; //should never reach
+}
+
+bool my_str_cmpr(char* str1, char* str2){
+	while(str1 != NULL && str2 != NULL && *str1 != '\0' && *str2 != '\0'){
+		if (*str1 != *str2)
+			return false;
+		str1++;
+		str2++;
+	}
+	return true;
+}
+
+struct labels* get_label(char* label_name){
+	struct labels* tmp = label;
+	while (tmp != NULL) {
+		if ( tmp->label_name != NULL && my_str_cmpr(tmp->label_name,label_name))
+			return tmp;
+		//printf("%s is not %s\n",tmp->label_name,label_name);
+		tmp = tmp->next;
+	}
+
+	return NULL;
 }
 
 void first_pass() {
@@ -118,86 +172,86 @@ void first_pass() {
 	int row = 0;
 
 	//for every line of code inputed:
-	while (fgets(buffer, sizeof(buffer), program) != NULL) {
+	while(fgets(buffer, sizeof(buffer), program) != NULL) {
         
 		if (is_line_a_label(buffer)) {
 			//save the label name to row number.
-            printf("buff: %s\n",buffer);
-			save_label(buffer, row + 1);//I think maybe save it with row, and not row+1? and not inc row. since we do not want to count these rows in second pass either. todo: DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN
+            //printf("buff: %s\n",buffer);
+			save_label(buffer, row); //The current row and not row + 1
             
 		}
 		else {
-            printf("row: %d\n",row);
+            //printf("row: %d\n",row);
 			row++; //instruction
 			//todo : maybe check if the line is actually an instruction
 		}
 	}
 }
-char* handle_opcode(char* str){
-	if (strcomp(str, "add") == 0){
+char* handle_opcode(char** str){
+	if (strcmp(*str, "add") == 0){
 		return ADD;
 	}
-	else if (strcomp(str, "sub") == 0){
+	else if (strcmp(*str, "sub") == 0){
 		return SUB;
 	}
-	else if (strcomp(str, "mac") == 0){
+	else if (strcmp(*str, "mac") == 0){
 		return MAC;
 	}
-	else if (strcomp(str, "and") == 0){
+	else if (strcmp(*str, "and") == 0){
 		return AND;
 	}
-	else if (strcomp(str, "or") == 0){
+	else if (strcmp(*str, "or") == 0){
 		return OR;
 	}
-	else if (strcomp(str, "xor") == 0){
+	else if (strcmp(*str, "xor") == 0){
 		return XOR;
 	}
-	else if (strcomp(str, "sll") == 0){
+	else if (strcmp(*str, "sll") == 0){
 		return SLL;
 	}
-	else if (strcomp(str, "sra") == 0){
+	else if (strcmp(*str, "sra") == 0){
 		return SRA;
 	}
-	else if (strcomp(str, "srl") == 0){
+	else if (strcmp(*str, "srl") == 0){
 		return SRL;
 	}
-	else if (strcomp(str, "beq") == 0){
+	else if (strcmp(*str, "beq") == 0){
 		return BEQ;
 	}
-	else if (strcomp(str, "bne") == 0){
+	else if (strcmp(*str, "bne") == 0){
 		return BNE;
 	}
-	else if (strcomp(str, "blt") == 0){
+	else if (strcmp(*str, "blt") == 0){
 		return BLT;
 	}
-	else if (strcomp(str, "bgt") == 0){
+	else if (strcmp(*str, "bgt") == 0){
 		return BGT;
 	}
-	else if (strcomp(str, "ble") == 0){
+	else if (strcmp(*str, "ble") == 0){
 		return BLE;
 	}
-	else if (strcomp(str, "bge") == 0){
+	else if (strcmp(*str, "bge") == 0){
 		return BGE;
 	}
-	else if (strcomp(str, "jal") == 0){
+	else if (strcmp(*str, "jal") == 0){
 		return JAL;
 	}
-	else if (strcomp(str, "lw") == 0){
+	else if (strcmp(*str, "lw") == 0){
 		return LW;
 	}
-	else if (strcomp(str, "sw") == 0){
+	else if (strcmp(*str, "sw") == 0){
 		return SW;
 	}
-	else if (strcomp(str, "reti") == 0){
+	else if (strcmp(*str, "reti") == 0){
 		return RETI;
 	}
-	else if (strcomp(str, "in") == 0){
+	else if (strcmp(*str, "in") == 0){
 		return IN;
 	}
-	else if (strcomp(str, "out") == 0){
+	else if (strcmp(*str, "out") == 0){
 		return OUT;
 	}
-	else if (strcomp(str, "halt") == 0){
+	else if (strcmp(*str, "halt") == 0){
 		return HALT;
 	}
 	else {//error
@@ -205,53 +259,53 @@ char* handle_opcode(char* str){
 	}
 }
 
-char* handle_register(char* str){
-	if (strcomp(str, "$zero") == 0){
+char* handle_register(char** str){
+	if (strcmp(*str, "$zero") == 0){
 		return $ZERO;
 	}
-	else if (strcomp(str, "$imm1") == 0){
+	else if (strcmp(*str, "$imm1") == 0){
 		return $IMM1;
 	}
-	else if (strcomp(str, "$imm2") == 0){
+	else if (strcmp(*str, "$imm2") == 0){
 		return $IMM2;
 	}
-	else if (strcomp(str, "$v0") == 0){
+	else if (strcmp(*str, "$v0") == 0){
 		return $V0;
 	}
-	else if (strcomp(str, "$a0") == 0){
+	else if (strcmp(*str, "$a0") == 0){
 		return $A0;
 	}
-	else if (strcomp(str, "$a1") == 0){
+	else if (strcmp(*str, "$a1") == 0){
 		return $A1;
 	}
-	else if (strcomp(str, "$a2") == 0){
+	else if (strcmp(*str, "$a2") == 0){
 		return $A2;
 	}
-	else if (strcomp(str, "$t0") == 0){
+	else if (strcmp(*str, "$t0") == 0){
 		return $T0;
 	}
-	else if (strcomp(str, "$t1") == 0){
+	else if (strcmp(*str, "$t1") == 0){
 		return $T1;
 	}
-	else if (strcomp(str, "$t2") == 0){
+	else if (strcmp(*str, "$t2") == 0){
 		return $T2;
 	}
-	else if (strcomp(str, "$s0") == 0){
+	else if (strcmp(*str, "$s0") == 0){
 		return $S0;
 	}
-	else if (strcomp(str, "$s1") == 0){
+	else if (strcmp(*str, "$s1") == 0){
 		return $S1;
 	}
-	else if (strcomp(str, "$s2") == 0){
+	else if (strcmp(*str, "$s2") == 0){
 		return $S1;
 	}
-	else if (strcomp(str, "$gp") == 0){
+	else if (strcmp(*str, "$gp") == 0){
 		return $GP;
 	}
-	else if (strcomp(str, "$sp") == 0){
+	else if (strcmp(*str, "$sp") == 0){
 		return $SP;
 	}
-	else if (strcomp(str, "$ra") == 0){
+	else if (strcmp(*str, "$ra") == 0){
 		return $RA;
 	}
 	else{//error
@@ -259,56 +313,79 @@ char* handle_register(char* str){
 	}
 }
 
-void handle_immediate(char* str, char* outpointer){
-	if (immediate_is_label(str)){//if is label, return value of label
-		return get_label_value(str);
-	} //else, return value of immediate in hex, 12B length
-	int strvalue = strtol(str, NULL, 10);
-	int formattedStrResult = asprintf(&outpointer, "%012X", strvalue); //THIS FUNC CONTAINS IMPLICIT MALLOC, MEM IS FREED IN translate_instruction()
-	printf("outpointer is %s", outpointer);//DEBUG
-	//todo: check if this prints correctly with 0x or without (we want it without)
-	if(formattedStrResult > 0){	
-    	return;
-	} else {
-    	// todo some error?
+void handle_immediate(char* str, char* hex){
+	struct labels* lbl = get_label(str);
+	if (lbl != NULL){ // If it's a label
+		//printf("lbl: %s\n", lbl->label_name);
+		snprintf(hex, 12,"%3X", lbl->label_address);
+		for (int i = 0; i < 3; i++){
+			if (hex[i] == ' ')
+				hex[i] = '0';
+		}
+		return;
+	}
+	//else, return value of immediate in hex, 12B length
+	int strvalue = strtol(str, NULL, 10); // convert to decimal
+	snprintf(hex, 12,"%3X", strvalue); //THIS FUNC CONTAINS IMPLICIT MALLOC, MEM IS FREED IN translate_instruction()
+	for (int i = 0; i < 3; i++){
+		if (hex[i] == ' ')
+			hex[i] = '0';
+	}
+	return;
+}
+
+void translate_instruction(char* line, char* instruction) {
+	int wordnum = 0;
+	//char instruction[48] = {0};
+	char* pch = "";
+	char immediate[3] = {0};
+	for (wordnum = 0; wordnum < NUM_OF_PARAMS; wordnum++){// can use for loop since non-label lines always have 6 params
+		if (wordnum == 0){
+			line = line+1; // avoid the tab
+			pch = strtok(line, "\t, $");
+		}else{
+			pch = strtok(NULL, ", #");
+			// printf("%d strtok pch:%s\n",wordnum,pch);
+		}
+		if (wordnum == 0){//first param, is opcode
+			strcat(instruction, handle_opcode(&pch));
+			// printf("%s @@@\n",instruction);
+		}
+	 	else if (1 <= wordnum && wordnum <= 4){//1-4 param, register
+			strcat(instruction, handle_register(&pch));
+			// printf("%s @@@\n",instruction);
+		}
+		else{//5-6 param, immediates
+			handle_immediate(pch, immediate);
+			// printf("%s ###\n",immediate);
+			int last_three_bytes = strlen(immediate) - 3;
+			strcat(instruction, &(immediate[last_three_bytes]));
+			// printf("%s ###\n",instruction);
+		}
 	}
 }
 
-char* translate_instruction(char* line) {
-	int wordnum = 0;
-	char* instruction = "";
-	char* pch;
-	char* currword = "";
-	char* temptofree;
-	for (wordnum = 0; wordnum < NUM_OF_PARAMS; wordnum++){// can use for loop since non-label lines always have 6 params
-		pch = strtok(line, " ,$"); //need to change this since it is incorrect, maybe only go by " ,"? todo: DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN
-		if (wordnum == 0){//first param, is opcode
-			instruction = strcat(instruction, handle_opcode(pch));
-		}
-	 	else if (1 <= wordnum || wordnum <= 4){//1-4 param, register
-			instruction = strcat(instruction, handle_register(pch));
-		}
-		else{//5-6 param, immediates
-			handle_immediate(pch, temptofree);
-			instruction = strcat(instruction, temptofree);
-			free(temptofree);//mem allocated in handle_immediate
-		}
-	}
-	return instruction;
+void writetoimemin(char* nextline, int row){
+	fseek ( imemin , 0, SEEK_CUR);
+	fputs( nextline, imemin ); 
+	fputs( "\n", imemin );
 }
 
 void second_pass() {
 	/* the second pass already has the linked list of all the lables,
 	now we can create the two output files.*/
 	char buffer[MAX_LINE_LEN];
-	char* nextline;
 	int row = 0;
 	//for every line of code inputed:
 	while (fgets(buffer, sizeof(buffer), program) != NULL) {
 		if (!is_line_a_label(buffer)) {
 			//translate instruction to hex code:
-			nextline = translate_instruction(buffer);
-			writetoimemin(nextline, row);
+			//printf("translating row %d\n",row);
+			char* instruction = (char*) calloc(sizeof(char),48); // todo add check
+			translate_instruction(buffer, instruction);
+			printf("%s\n",instruction);
+			writetoimemin(instruction, row);
+			free(instruction);
 			row++;
 		}
 	}
@@ -337,7 +414,7 @@ int main(int argc, char* argv[]) {
 
 	/* first pass- remmbering lables */
 	first_pass();
-
+	//printf("First pass is done.\n");
 	program = fopen(argv[1], "r"); // read the file again.
 	if (program == NULL) {
 		return EXIT_FAILURE;
@@ -345,7 +422,10 @@ int main(int argc, char* argv[]) {
 
 	second_pass();
 
-
+	//todo free the linked list
+	fclose(program);
+	fclose(imemin);
+	fclose(dmemin);
 
 	return 0;
 }
